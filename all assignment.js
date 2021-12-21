1ec1012aeaf483b53ad4697e5cbc73d9ad6fb23f
skakil mia <dewanshakil88@gmail.com>
2021-12-21 18:52:57 +0600

    update all file

---
+++ all assignment.js
@@ -0,0 +1,33 @@
+//email pattern=======
+
+// let txt="dewanshail88@gmail.com";
+// let pattern=/^[a-zA-Z0-9]*@[a-z]*\.[a-z]{2,5}$/;
+// console.log(pattern.test(txt));
+
+//username pattern======
+
+let username="shakil-";
+let pattern= /^[a-z\-]*$/;
+console.log(pattern.test(username));
+
+//Bangladeshi phone number pattern===
+
+// let number="+8801851246924";
+// let pattern= /^(01|\+8801|8801)[0-9]{9}$/;
+// console.log(pattern.test(number));
+
+// password pattern =====
+
+// let password="shakil5124";
+// let pattern= /^[a-zA-Z0-9!@#$%]{6,16}$/;
+// console.log(pattern.test(password));
+
+//zipcode pattern  =====
+
+// let zipcode="1234";
+// let pattern= /^\d{4}$/;
+// console.log(pattern.test(zipcode));
+
+
+
+
